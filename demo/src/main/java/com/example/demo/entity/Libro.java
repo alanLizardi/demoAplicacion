1dadb77128d433b9773aa73b5be992d002e0a864
Alan.Lizardi <alan.lizardi.albizu@accenture.com>
2019-12-22 16:58:53 -0300

    restauracion

--- demo/src/main/java/com/example/demo/entity/Libro.java
+++ demo/src/main/java/com/example/demo/entity/Libro.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +9,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Libro {
+public class Libro implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3456200378537261926L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -69,6 +76,61 @@ public class Libro {
 
 	public void setIdCategoria(int idCategoria) {
 		this.idCategoria = idCategoria;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ISBN;
+		result = prime * result + ((autor == null) ? 0 : autor.hashCode());
+		result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + ((editorial == null) ? 0 : editorial.hashCode());
+		result = prime * result + idCategoria;
+		result = prime * result + ((nombreLibro == null) ? 0 : nombreLibro.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		if (ISBN != other.ISBN)
+			return false;
+		if (autor == null) {
+			if (other.autor != null)
+				return false;
+		} else if (!autor.equals(other.autor))
+			return false;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (editorial == null) {
+			if (other.editorial != null)
+				return false;
+		} else if (!editorial.equals(other.editorial))
+			return false;
+		if (idCategoria != other.idCategoria)
+			return false;
+		if (nombreLibro == null) {
+			if (other.nombreLibro != null)
+				return false;
+		} else if (!nombreLibro.equals(other.nombreLibro))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Libro [ISBN=" + ISBN + ", nombreLibro=" + nombreLibro + ", editorial=" + editorial + ", descripcion="
+				+ descripcion + ", autor=" + autor + ", idCategoria=" + idCategoria + "]";
 	}
 
 }
